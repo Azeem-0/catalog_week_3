cbcbb1cbfbf00d191cbccbad34b81213738cb2a3
Azeem Shaik <azeemshaik025@gmail.com>
2024-10-22 10:49:54 +0530

    initialize project structure

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod models;
+pub mod repository;
+pub mod routes;
+pub mod services;
+
+pub async fn run() -> std::io::Result<()> {
+    // init database
+
+    //init http server and return it to main function.
+
+    print!("Welcome");
+
+    Ok(())
+}
